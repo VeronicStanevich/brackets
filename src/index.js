07cb191ef923cf164853932d7e3bb24a6d4048a4
veranika <veronica.stanevich@gmail.com>
2019-09-28 18:24:21 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsStack = [];
+    const stringChars = str.split('');
+    const bracketsMap = Object.fromEntries(bracketsConfig);
+
+    for (const char of stringChars) {
+        const isOpenCharSameAsClose = bracketsMap[char] === char;
+
+        if (bracketsMap[char] &&
+            !(isOpenCharSameAsClose && bracketsStack.includes(char))) {
+            bracketsStack.push(char);
+        } else {
+            const lastBracket = bracketsStack.pop();
+            if (char !== bracketsMap[lastBracket]) {
+                return false;
+            }
+        }
+    }
+
+    return !bracketsStack.length;
+};
